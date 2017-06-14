538fb2d8478fb29800f5ba31a646c35e993c54e4
Steven Mitchell <smitch15@binghamton.edu>
2017-06-14 16:05:18 -0400

    update server address hostname

--- newClient.py
+++ newClient.py
@@ -2,22 +2,27 @@
 import socket
 import sys
 import serial
+
 # Create a TCP/IP socket
+# TCP (not UDP) --> bidirectional socket connection from host to client 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print "Initialized..."
+# print "init success"
+
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.0.102', 10004)
+server_address = ('192.168.0.101', 10004)
+
 #use command ifconfig on HOST computer to get IP addresss
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
 #ser = serial.Serial('/dev/ttyACM0',9600)
 ser = serial.Serial('/dev/serial/by-id/usb-Arduino_Srl_Arduino_Uno_75436343430351607011-if00',9600)
+
 #ser = serial.Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_755333433363511061A0-if00', 9600)
 #this commented out line used to work but stopped for whatever reason.
 #Google that line with fix if the new line doens't work anymore
 
-print "GOt here"
+print "Got here"
 ser.write('0')
 while True:
     print "YOOOOOO World"
