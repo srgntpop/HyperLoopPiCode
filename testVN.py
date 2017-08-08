d37a85517dc2693b70a06fdd51ba5967740f9f5d
CrazyWonton <kevintarc13@gmail.com>
2017-08-08 17:32:12 -0400

    Updating testVN for velocity and position
    added some extra stuff to test for velocity and position

--- testVN.py
+++ testVN.py
@@ -1,48 +1,91 @@
 from vnpy import *
-import time as time
-#from scipy import *
 
-#def medianFilter(array):
-	#array = sorted array(array)
-	#return array(25)
+import time as time
+
+
+# from scipy import *
+
+
+
+# def medianFilter(array):
+
+# array = sorted array(array)
+
+# return array(25)
+
+
 
 def medianFilter(arrayIn):
-	#sorted: worst case O(nlogn)
-	median = 0.0
-	sortedArrayIn = sorted(arrayIn)
-	if (len(sortedArrayIn) % 2 == 1):
-		median = sortedArrayIn[(len(sortedArrayIn)-1)/2]
-	else:
-		median = sortedArrayIn[len(sortedArrayIn)/2]
-	return median
-		
+    # sorted: worst case O(nlogn)
+
+    median = 0.0
+
+    sortedArrayIn = sorted(arrayIn)
+
+    if (len(sortedArrayIn) % 2 == 1):
+
+        median = sortedArrayIn[(len(sortedArrayIn) - 1) / 2]
+
+    else:
+
+        median = sortedArrayIn[len(sortedArrayIn) / 2]
+
+    return median
+
 
 def meanFilter(arrayIn):
-	count = 0
-	sum = 0.0
-	for i in range(len(arrayIn)/2):
-		sum += arrayIn[i] + arrayIn[len(arrayIn)-i-1]	
-		count += 1
-	if (len(arrayIn) % 2 == 1):
-		sum += arrayIn[(len(arrayIn)-1)/2] 
-	mean = sum/float(count)
-	return mean
+    count = 0
+
+    sum = 0.0
+
+    for i in range(len(arrayIn) / 2):
+        sum += arrayIn[i] + arrayIn[len(arrayIn) - i - 1]
+
+        count += 1
+
+    if (len(arrayIn) % 2 == 1):
+        sum += arrayIn[(len(arrayIn) - 1) / 2]
+
+    mean = sum / float(count)
+
+    return mean
+
 
 def main():
-	s = VnSensor()
-	s.connect('/dev/ttyUSB0', 115200)
-	reg = s.read_yaw_pitch_roll_magnetic_acceleration_and_angular_rates()
-	velocityX = 0.0
-	startTime = time.time()
-	timesRead = 0
-	while True:
-		timePassed = time.time() - startTime
-		if (timePassed < 2.0):
-			timesRead += 1
-			print(reg.accel.x)
-		else:
-			break
-	print("time per loop: ")
-	print(timePassed/timesRead)
+    s = VnSensor()
+
+    s.connect('/dev/ttyUSB0', 115200)
+
+    reg = s.read_yaw_pitch_roll_magnetic_acceleration_and_angular_rates()
+
+    velocityX = 0.0
+    position = 0.0
+    deltaV = 0.0
+    acceleration = 0.0
+    deltaT = 0.0
+    startTime = time.time()
+
+    timesRead = 0
+
+    while True:
+
+        timePassed = time.time() - startTime
+        deltaT = timePassed/timesRead
+        if (timePassed < 5.0):
+            timesRead+=1
+            acceleration = reg.accel.x
+            velocityX = velocityX + acceleration*deltaT
+            position = velocityX*deltaT + (acceleration/2)*(deltaT*deltaT)
+
+        if (timePassed % 1 == 0):
+            print(reg.accel.x, velocityX, position)
+
+        else:
+            break
+
+    print("time per loop: ")
+
+    print(timePassed / timesRead)
+
 
 main()
